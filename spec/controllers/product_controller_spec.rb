af74d1b5a821b7b17aa54899303106894c191b53
Daniel Nordstrom <d@nintera.com>
2011-11-06 02:23:07 +0100

    Adding POST request tests, and comments.

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -20,7 +20,7 @@ describe ProductController do
   end
   
   it "should respond with success status" do
-    response = get "/product/1.xml"
+    response = get "/product/1.xml", { 'sample' => 'test' }
     status_of(response).should == 200
   end
   
@@ -74,5 +74,86 @@ describe ProductController do
     
     products = document.css("product")
     products.count.should == 2
+  end
+  
+  it "should change product title" do
+    product(:id => 3)
+    
+    post "/product/3", { "title" => "New Title" }
+    
+    product = Product.get(3)
+    title   = product.title
+    product.destroy
+    
+    title.should == "New Title"
+  end
+  
+  it "should chance product price" do
+    product(:id => 3)
+    
+    post "/product/3", { "price" => "2000" }
+    
+    product = Product.get(3)
+    price   = product.price
+    product.destroy
+    
+    price.should == 2000
+  end
+  
+  it "should change product description" do
+    product(:id => 3)
+    
+    post "/product/3", { "description" => "New description" }
+    
+    product = Product.get(3)
+    description   = product.description
+    product.destroy
+    
+    description.should == "New description"
+  end
+  
+  it "should not change product ID" do
+    post "/product/1", { "id" => "5" }
+    
+    Product.get(1).should_not be_nil
+  end
+  
+  it "should respond to POST request with XML" do
+    response = post "/product/1"
+    format_of(response).should == :xml
+  end
+  
+  it "should include product ID as XML in POST response" do
+    response = post "/product/1"
+    document = Nokogiri::XML( content_of(response) )
+    
+    product = document.at_css("product")
+    id      = product.attribute("id").value.to_i
+    
+    id.should == 1
+  end
+  
+  it "should include product title as XML in POST response" do
+    response = post "/product/1"
+    document = Nokogiri::XML( content_of(response) )
+    
+    title = document.at_css("title").content
+    title.should == 'Sample Product'
+  end
+  
+  it "should include product price as XML in POST response" do
+    response = post "/product/1"
+    document = Nokogiri::XML( content_of(response) )
+    
+    price = document.at_css("price").content.to_i
+    price.should == 1000
+  end
+  
+  it "should include product description as XML in POST response" do
+    response = post "/product/1"
+    document = Nokogiri::XML( content_of(response) )
+    
+    description = document.at_css("description").content
+    description.should == "Sample description"
   end
 end
