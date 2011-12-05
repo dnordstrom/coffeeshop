d9f14bb63d62cf22169f4963c686e003bdec0bdc
Daniel Nordstrom <d@nintera.com>
2011-12-05 20:27:09 +0100

    Refactoring MarkupHelper.

--- lib/coffeeshop/helpers/markup_helper.rb
+++ lib/coffeeshop/helpers/markup_helper.rb
@@ -1,54 +1,109 @@
 module CoffeeShop
   module MarkupHelper
-    def link_to(title, resource)
-      markup = ""
+    def link_to(title, resource, attributes = {})
+      href = CoffeeShop::Application.base
 
       case resource
       when Symbol
-        markup = link_to_page(title, resource)
+        href += "/page/#{resource.to_s}"
+      when Class
+        if resource.is_a? CoffeeShop::Model
+          href += "/#{resource.class.downcase}/#{resource.id}"
+        end
+      end
+
+      attributes = {
+        href: href,
+        title: title
+      }.merge(attributes)
+      
+      markup = open_tag(:a, attributes)
+      markup += title
+      markup += close_tag(:a)
+    end
+
+    def button_to(value, controller, method)
+      form_attributes = {
+        action: CoffeeShop::Application.base + "/#{controller.to_s}",                
+        method: "post",
+      }
+      
+      markup = open_tag(:form, form_attributes)
+      markup += hidden_field(:_method, { value: method.to_s })
+      markup += submit_button(value)
+      markup += close_tag(:form)
+    end
+
+    def text_field(name, attributes = {})
+      attributes = {
+        type: "text",
+        name: name.to_s,
+        id: name.to_s
+      }.merge(attributes)
+      
+      open_tag(:input, attributes)
+    end
+
+    def hidden_field(name, attributes = {})
+      aatributes = {
+        type: "text",
+        name: name.to_s,
+        id: name.to_s
+      }.merge(attributes)
+
+      open_tag(:input, attributes)
+    end
+
+    def submit_button(value, attributes = {})
+      attributes = {
+        type: "submit",
+        value: value
+      }.merge(attributes)
+
+      open_tag(:input, attributes)
+    end
+
+    def begin_form(model, method, attributes = {})
+      # Accepts both object and symbol argument for model.
+      # "case model" will check the type of the model argument.
+      case model
+      when Symbol
+        action = mode.to_s
+      when Class
+        action = model.class.downcase
+      end
+
+      attributes = {
+        method: "post",
+        action: CoffeeShop::Application.base + "/#{action}"
+      }.merge(attributes)
+      
+      open_tag(:form, attributes) +
+        close_tag(:form)
+    end
+
+    def end_form
+      close_tag(:form)
+    end
+
+    def open_tag(tag, attributes = {})
+      markup = "<#{tag.to_s}"
+      markup += format_attributes(attributes) if attributes.length > 0
+      markup += ">"
+    end
+
+    def format_attributes(attributes)
+      markup = ""
+
+      attributes.each do |key, value|
+        markup += " #{key.to_s}=\"#{value}\""
       end
 
       markup
     end
 
-    def link_to_page(title, page)
-      '<a href="' +
-        CoffeeShop::Application.base +
-        '/' + 'page/' + page.to_s +
-        '" title="' + title + '">' + title + '</a>'
-    end
-
-    def button_to(value, controller, method)
-      '<form action="' + CoffeeShop::Application.base + '/' + controller.to_s + '" method="post">' +
-        '<input type="hidden" name="_method" id="_method" value="' + method.to_s + '">' +
-        '<input type="submit" value="' + value + '">' +
-        '</form>'
-    end
-
-    def text_field(name, attributes)
-      markup =
-        '<input type="text" name="' + name.to_s + '" id="' + name.to_s + '"'
-
-      attributes.each do |key, value|
-        markup += ' ' + key.to_s + '="' + value.to_s + '"'
-      end
-        
-      markup + '>'
-    end
-
-    def begin_form(model, method, attributes)
-      markup = '<form action="' +
-        CoffeeShop::Application.base + '/' + model.to_s + '"' + ' method="post"'
-
-      attributes.each do |key, value|
-        markup += ' ' + key.to_s + '="' + value + '"'
-      end
-
-      markup + '>'
-    end
-
-    def end_form
-      '</form>'
+    def close_tag(tag)
+      "</#{tag.to_s}>"
     end
   end
 end
--- spec/helpers/markup_helper_spec.rb
+++ spec/helpers/markup_helper_spec.rb
@@ -7,43 +7,18 @@ include MarkupHelper
 
 describe MarkupHelper do
   before :all do
+    @base = CoffeeShop::Application.base
+
     setup
   end
 
-  it "should output an anchor tag linking to a page" do
-    href = CoffeeShop::Application.base + "/page/index"
-    title = "Home"
-
-    link_to(title, :index).should ===
-      '<a href="' + href + '" title="' + title + '">' + title + '</a>'
-  end
-
-  it "should output a button to POST Session.delete" do
-    action = CoffeeShop::Application.base + "/session"
-    value = "Log out"
-    controller = :session
-    method = :delete
-
-    button_to(value, controller, method).should ===
-      '<form action="' + action.to_s + '" method="post">' +
-      '<input type="hidden" name="_method" id="_method" value="' + method.to_s + '">' +
-      '<input type="submit" value="' + value + '">' +
-      '</form>'
-  end
-
-  it "should output a text field with the specified attributes" do
-    text_field(:setting, class: "test").should === '<input type="text" ' +
-      'name="setting" id="setting" class="test">'
-  end
-
-  it "should output a POST form tag with the specified values" do
-    action = CoffeeShop::Application.base + '/setting'
-
-    begin_form(:setting, :post, id: "setting").should ===
-      '<form action="' + action + '" method="post" id="setting">'
-  end
-
-  it "should output a closing form tag" do
-    end_form.should === "</form>"
+  it "should output an anchor tag linking to index page" do
+    markup = link_to("Home", :index)
+    
+    (
+      markup.include?("href=\"#{@base}/page/index\"") &&
+      markup.include?("title=\"Home\"") &&
+      markup.include?(">Home</a>")
+    ).should be_true
   end
 end
